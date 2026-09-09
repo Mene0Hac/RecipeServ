79b76e2248eb942c8887c098702ed89553c3b117
Mene0Hac <menirxac@mail.ru>
2026-09-09 15:36:08 +0300

    test

--- ping.py
+++ ping.py
@@ -1,15 +1,12 @@
 import requests
 
-URL = "http://127.0.0.1:5000/get_all_users"
+URL = "http://127.0.0.1:5000/"
 
 try:
-    response = requests.get(URL)
+    response = requests.get(URL+"get_all_users")
     print("Статус:", response.status_code)
     for row in response.json():
         print(f"ID: {row['id']}, Имя: {row['name']}, Возраст: {row['age']}"+"\n")
-
-    
-
 
 except requests.exceptions.ConnectionError:
     print("Ошибка: сервер не запущен или недоступен")
